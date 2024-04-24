597daf3b60d902548617670e01711ecb70e53932
gerard-inspedralbes <122524717+gerard-inspedralbes@users.noreply.github.com>
2024-04-24 12:13:04 +0200

    Update Ex1.java
    afegida solució Ex1

--- src/Ex1.java
+++ src/Ex1.java
@@ -10,7 +10,49 @@ a continuació per a generar l'excepció NumberFormatException:
  */
 
 public class Ex1 {
+/**
+ *
+ * @author ausias
+ */
+public class E1 {
+    static Scanner s = new Scanner(System.in);
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean correcte = false;
+        while(!correcte){
+            correcte = demanarNumerosPerDividir();
+        }
     }
+
+    static boolean demanarNumerosPerDividir() {
+        int num1 = 0;
+        int num2 = 0;
+        boolean correcte = false;
+        try{
+            System.out.print("Num1: ");
+            num1 = Integer.parseInt(s.next());
+            System.out.print("Num2: ");
+            num2 = Integer.parseInt(s.next());
+            correcte = true;
+            calcularDivisio(num1, num2);
+        }catch(NumberFormatException e){
+            System.out.println("Els nombres introduïts no son correctes");
+        }
+        
+        return correcte;
+    }
+
+    private static void calcularDivisio(int num1, int num2) {
+        int result = 0;
+        try{
+            result = num1 / num2;
+            System.out.println(result);
+        }catch(ArithmeticException e){
+            System.out.println("Divisió entre zero!!!");
+        }
+    }
+    
 }
