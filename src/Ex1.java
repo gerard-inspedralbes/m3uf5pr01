5073fad6846ebaabfab07e5186079182d9ce951d
isard <gtorrents@inspedralbes.cat>
2024-04-24 11:47:21 +0200

    initial commit

---
+++ src/Ex1.java
@@ -0,0 +1,16 @@
+/*
+1. Programar una classe que divideixi dos números sencers demanats a l'usuari per teclat
+i mostri per pantalla el resultat (aritmètica sencera). Capturar un possible
+error de conversió de cadena a número (excepció NumberFormatException),
+en aquest cas cal convidar a l'usuari a introduir de nou el número.
+Capturar també la possible divisió entre zero (excepció ArithmeticException)
+informant a l'usuari de l'error. Cal llegir les dades de teclat tal com s’indica
+a continuació per a generar l'excepció NumberFormatException:
+            num = Integer.parseInt(input.next());
+ */
+
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
